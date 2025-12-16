81e63d8ea3275f2311ad93cea82bf9f6f2f564f6
Macbook <macbook@Macbooks-MacBook-Air.local>
2025-12-16 13:41:04 +0545

    use ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import { useState, useEffect } from 'react'
-
-async function getData() {
-  const url = "https://jsonplaceholder.typicode.com/todas/1";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    throw error
-  }
-}
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [todos, setTodos] = useState([])
-  const [error, setError] = useState(null)
-  useEffect(() => {
-    async function logData() {
-      try {
-        console.log(await getData());
-      }catch (error) {
-        setError(error)
-        console.error("Error fetching data:", error);
-      }
-    }
-    logData();
-  }, []);
+  const h1Ref =  useRef(0)
+  h1Ref.current = 1
+  console.log(h1Ref);
+  const inputRef = useRef()
+  function handleClick() {
+    inputRef.current.focus();
+  }
   return (
-    <>
-      <h1>{count}</h1>
-      {error && <p style={{ color: 'red' }}>Error: {error.message}</p>}
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </>
-  )
+    <div>
+      <h1>Hello, World!</h1>
+      <button onClick={()=> {handleClick()}}>Click me</button>
+      <input type="text" placeholder="type something" ref={inputRef} />
+    </div>
+  );
 }
 
-export default App
+export default App; 
