263ad61da432f67d500cf1e742c060f905c0aeba
Macbook <macbook@Macbooks-MacBook-Air.local>
2025-12-19 02:03:41 +0545

    types and interface

---
+++ src/typescript.tsx
@@ -0,0 +1,14 @@
+type TValue = string | number;
+type Tbolean = boolean;
+
+const a = 5;
+const b: string = 123;
+
+
+
+
+function App(){
+    return <h1>Hello, TypeScript with React!</h1>;      
+}
+
+export default App;
