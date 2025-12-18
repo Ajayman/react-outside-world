59966fc9aa934753247fa2ccef8e37c9c2f507ba
Macbook <macbook@Macbooks-MacBook-Air.local>
2025-12-18 06:19:30 +0545

    caching, use Memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useState,useMemo } from 'react'
 function App() {
-const [name, setName] = useState('Kumar')  
-const derivedName = name;
+  const [name, setName] = useState('Kumar');
+  const derivedName = name;
+  const [users, setUsers] = useState([
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+  ]);
+  const idtouser = useMemo(() => {
+    const foundUser = users.find((user) => {
+      console.log("find chalyo")
+      if (user.id === 2) {
+        return user.name
+      }
+    }
+    )
+    return foundUser;
+  }, [users])
   return (
-    <>
-      {derivedName}
-    </>
+    <div>
+      <h2>{derivedName}</h2>
+      <button onClick={() => setName('Bijay')}>Change name</button>
+    </div>
   )
 }
 
